e8f1a914aad67cec3033a83ef759bfc7b37363d9
spacezyx <1292171824@qq.com>
2022-03-28 20:41:02 +0800

    zz's checkpoint

--- src/main/java/cn/edu/sjtu/ist/ecssbackendedge/repository/TaskStateRepository.java
+++
@@ -1,8 +0,0 @@
-//package cn.edu.sjtu.ist.ecssbackendedge.repository;
-//
-//import cn.edu.sjtu.ist.ecssbackendedge.entity.znew.MyTask;
-//import cn.edu.sjtu.ist.ecssbackendedge.entity.znew.TaskState;
-//import org.springframework.data.jpa.repository.JpaRepository;
-//
-//public interface TaskStateRepository  extends JpaRepository<TaskState,Integer> {
-//}
